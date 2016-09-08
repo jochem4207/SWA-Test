f656fbd693e045475d23128179c8a60baf5d1197
Jochem <jochemdekruijf93@gmail.com>
2016-09-08 14:53:21 +0200

    This is it!

---
+++ DemoAndTestProject/DemoAndTestProject/MyClass.cpp
@@ -0,0 +1,19 @@
+#include "MyClass.h"
+
+
+
+MyClass::MyClass()
+{
+}
+
+
+MyClass::~MyClass()
+{
+}
+
+
+// Some Funny Class
+int MyClass::hello(int pizza, float type)
+{
+	return 0;
+}
---
+++ DemoAndTestProject/DemoAndTestProject/MyClass.h
@@ -0,0 +1,11 @@
+#pragma once
+class MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+	// Some Funny Class
+	static int hello(int pizza, float type);
+};
+
+
